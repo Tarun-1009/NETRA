bdf3c77ecafaf299cc99d30491cb6fb1042af86d
Shri Krishan <shrikrishan@Shris-MacBook-Air.local>
2026-01-16 01:41:29 +0530

    create hero section content for landing page

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -1,8 +1,44 @@
+import './Hero.css';
+
 function Hero() {
     return (
         <div>
             <section id="home">
-                <h1>Hero Section</h1>
+    <div class="container">
+        <main class="hero">
+            
+            <div class="parent">
+                <div class="circle">
+                    <img src='/netra.jpg' className="circle-img" alt="Profile" />
+                </div>
+            </div>
+            
+            
+            <div class="hero-text">
+                <h1><span class="blue-text">NETRA:</span> Neural Engine for Text & Reality Assistance.</h1>
+                <p>Empowering independence with AI-driven visual and text assistance.</p>
+                <button class="btn-start">Start Now</button>
+
+                <div class="features-grid">
+                    <div class="feature-item">
+                        <div class="icon">💰</div>
+                        <p>Intelligent Safety & Assistance</p>
+                    </div>
+                    <div class="feature-item">
+                        <div class="icon">📄</div>
+                        <p>Smart Reader</p>
+                    </div>
+                    <div class="feature-item">
+                        <div class="icon">👁️</div>
+                        <p>Universal Object Recognition</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+    </div>
+
+
             </section>
         </div>
     )
